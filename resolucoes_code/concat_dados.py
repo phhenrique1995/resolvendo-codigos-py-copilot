0795cccaf5ebb44d72f89c3185f3d781814c6b63
Pedro Henrique Gomes dos Santos <ph-chagas@hotmail.com>
2025-05-13 14:52:10 +0000

    concatenar dados e repetir testeo

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,7 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+info1 = input("Digite a primeiro informação: ")
+info2 = input("Digite a segundo informação: ")
+
+# Exibindo o resultado com um espaço entre os dados
+print("O resultado da concatenação é: ", info1 + " " + info2)
